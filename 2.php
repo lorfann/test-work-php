6f38a03a8244de76cf28006c65ec57a1c15417df
Evgeny Kholodnyak <samyray355@gmail.com>
2024-07-06 15:04:02 +0300

    реализовал функцию mySortForKey

--- 2.php
+++ 2.php
@@ -9,7 +9,7 @@ function convertString ( string $a, string $b) :string {
         
         while (($lastPosition = strpos($a, $b, $lastPosition)) !== false) {
             $positions[] = $lastPosition;
-            $lastPosition += mb_strlen($b);
+            $lastPosition += strlen($b);
         }
     
         // Проверка что  в строке $a содержится 2 и более подстроки $b
@@ -28,4 +28,26 @@ function convertString ( string $a, string $b) :string {
     
         return $a;
     
+}
+
+
+/*====================================================================================== */
+
+
+
+
+function mySortForKey($a, $b) {
+    // Проверка наличия ключа $b в каждом дочернем массиве
+    foreach ($a as $index => $childArray) {
+        if (!array_key_exists($b, $childArray)) {
+            throw new Exception("Ключ '$b' отсутствует в массиве с индексом $index.");
+        }
+    }
+
+    // Функция сравнения для usort
+    usort($a, function($elem1, $elem2) use ($b) {
+        return $elem1[$b] <=> $elem2[$b]; // Оператор <=> (космический корабль) сравнивает значения ключа $b в двух элементах массива $elem1 и $elem2. Он возвращает -1, 0 или 1, что используется для сортировки.
+    });
+
+    return $a;
 }
